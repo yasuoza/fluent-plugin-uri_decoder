cb1a0a2dbc23451744e0944d148f76ad95ef4546
Kenji Okimoto <okimoto@clear-code.com>
2017-01-06 17:17:56 +0900

    Add filter version

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require 'bundler/setup'
 require 'fluent/test'
 require 'fluent/test/helpers'
 require 'fluent/test/driver/output'
+require 'fluent/test/driver/filter'
 require 'test-unit'
 
 Test::Unit::TestCase.include(Fluent::Test::Helpers)
